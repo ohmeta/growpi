9b5c3bdfa9b9bdd93e87b938f2c8371f8a432beb
alienzj <alienchuj@gmail.com>
2022-12-26 16:51:38 +0800

    fix

--- growpi/corer.py
+++ growpi/corer.py
@@ -12,7 +12,7 @@ import growpi
 WORKFLOWS = ["grid_all", "all"]
 
 
-def run_snakemake(args, unknown, snakefile):
+def run_snakemake(args, unknown, snakefile, workflow):
     conf = growpi.parse_yaml(args.config)
 
     if not os.path.exists(conf["params"]["samples"]):
@@ -27,153 +27,116 @@ def run_snakemake(args, unknown, snakefile):
         args.config,
         "--cores",
         str(args.cores),
+        "--until",
+        args.task
     ] + unknown
 
-    if args.conda_create_envs_only:
+    if "--touch" in unknown:
+        pass
+    elif args.conda_create_envs_only:
         cmd += ["--use-conda", "--conda-create-envs-only"]
+        if args.conda_prefix is not None:
+            cmd += ["--conda-prefix", args.conda_prefix]
     else:
         cmd += [
-            "--rerun-incomplete",
             "--keep-going",
             "--printshellcmds",
             "--reason",
-            "--until",
-            args.task,
         ]
 
         if args.use_conda:
             cmd += ["--use-conda"]
+            if args.conda_prefix is not None:
+                cmd += ["--conda-prefix", args.conda_prefix]
 
         if args.list:
             cmd += ["--list"]
-        elif args.run:
-            cmd += [""]
+        elif args.run_local:
+            cmd += ["--local-cores", str(args.local_cores),
+                    "--jobs", str(args.jobs)]
+        elif args.run_remote:
+            cmd += ["--profile", args.profile,
+                    "--local-cores", str(args.local_cores),
+                    "--jobs", str(args.jobs)]
         elif args.debug:
-            cmd += ["--debug-dag", "--dry-run"]
-        elif args.dry_run:
+            cmd += ["--debug-dag"]
+        else:
             cmd += ["--dry-run"]
-        elif args.qsub:
-            cmd += [
-                "--cluster-config",
-                args.cluster,
-                "--jobs",
-                str(args.jobs),
-                "--latency-wait",
-                str(args.wait),
-                '--cluster "qsub -S /bin/bash -cwd \
-                -q {cluster.queue} -P {cluster.project} \
-                -l vf={cluster.mem},p={cluster.cores} \
-                -binding linear:{cluster.cores} \
-                -o {cluster.output} -e {cluster.error}"',
-            ]
+
+        if args.dry_run and ("--dry-run" not in cmd):
+            cmd += ["--dry-run"]
 
     cmd_str = " ".join(cmd).strip()
-    print(f"Running growpi:\n{cmd_str}")
+    print("Running growpi %s:\n%s" % (workflow, cmd_str))
 
     env = os.environ.copy()
     proc = subprocess.Popen(
-        cmd_str, shell=True, stdout=sys.stdout, stderr=sys.stderr, env=env,
+        cmd_str,
+        shell=True,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        env=env,
     )
     proc.communicate()
 
-
-class growpi_config:
-    sub_dirs = ["envs", "results", "logs/grid", "logs/grid_merge"]
-
-    def __init__(self, work_dir):
-        self.work_dir = os.path.realpath(work_dir)
-        self.growpi_dir = os.path.dirname(os.path.abspath(__file__))
-
-        self.config_file = os.path.join(self.growpi_dir, "config", "config.yaml")
-        self.cluster_file = os.path.join(self.growpi_dir, "config", "cluster.yaml")
-        self.snake_file = os.path.join(self.growpi_dir, "Snakefile")
-        self.envs_dir = os.path.join(self.growpi_dir, "envs")
-
-        self.new_config_file = os.path.join(self.work_dir, "config.yaml")
-        self.new_cluster_file = os.path.join(self.work_dir, "cluster.yaml")
-
-    def __str__(self):
-        message = """
-    ▄████  ██▀███   ▒█████   █     █░ ██▓███   ██▓
-    ██▒ ▀█▒▓██ ▒ ██▒▒██▒  ██▒▓█░ █ ░█░▓██░  ██▒▓██▒
-    ▒██░▄▄▄░▓██ ░▄█ ▒▒██░  ██▒▒█░ █ ░█ ▓██░ ██▓▒▒██▒
-    ░▓█  ██▓▒██▀▀█▄  ▒██   ██░░█░ █ ░█ ▒██▄█▓▒ ▒░██░
-    ░▒▓███▀▒░██▓ ▒██▒░ ████▓▒░░░██▒██▓ ▒██▒ ░  ░░██░
-    ░▒   ▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ░ ▓░▒ ▒  ▒▓▒░ ░  ░░▓  
-    ░   ░   ░▒ ░ ▒░  ░ ▒ ▒░   ▒ ░ ░  ░▒ ░      ▒ ░
-    ░ ░   ░   ░░   ░ ░ ░ ░ ▒    ░   ░  ░░        ▒ ░
-        ░    ░         ░ ░      ░              ░  
-                                                    
-
-           Omics for All, Open Source for All
-
-    Microbial commnuity grow rate profiling pipeline
-
-Thanks for using growpi.
-
-A metagenomics project has been created at %s
+    print(f'''\nReal running cmd:\n{cmd_str}''')
 
 
-if you want to create fresh conda environments:
+def update_config_tools(conf, begin, trimmer, rmhoster, profilers):
+    conf["params"]["simulate"]["do"] = False
+    conf["params"]["begin"] = begin
 
-        growpi run_wf --conda_create_envs_only
+    for trimmer_ in ["sickle", "fastp"]:
+        if trimmer_ == trimmer:
+            conf["params"]["trimming"][trimmer_]["do"] = True
+        else:
+            conf["params"]["trimming"][trimmer_]["do"] = False
 
-if you have environments:
+    for rmhoster_ in ["bwa", "bowtie2", "minimap2", "kraken2", "kneaddata"]:
+        if rmhoster_ == rmhoster:
+            conf["params"]["rmhost"][rmhoster_]["do"] = True
+        else:
+            conf["params"]["rmhost"][rmhoster_]["do"] = False
 
-        growpi --help
-        growpi init --help
-        growpi run_wf --help
-""" % (
-            self.work_dir
-        )
+    for profiler_ in ["irep", "grid", "demic"]:
+        if profiler_ in profilers:
+            conf["params"]["profiling"][profiler_]["do"] = True
+        else:
+            conf["params"]["profiling"][profiler_]["do"] = False
 
-        return message
+    if begin == "simulate":
+        conf["params"]["simulate"]["do"] = True
+    elif begin == "rmhost":
+        conf["params"]["trimming"][trimmer]["do"] = False
+    elif begin == "profiling":
+        conf["params"]["raw"]["save_reads"] = True
+        conf["params"]["raw"]["fastqc"]["do"] = False
+        conf["params"]["qcreport"]["do"] = False
 
-    def create_dirs(self):
-        """
-        create project directory
-        """
-        if not os.path.exists(self.work_dir):
-            os.mkdir(self.work_dir)
-
-        for sub_dir in growpi_config.sub_dirs:
-            os.makedirs(os.path.join(self.work_dir, sub_dir), exist_ok=True)
-
-        for i in os.listdir(self.envs_dir):
-            shutil.copyfile(
-                os.path.join(self.envs_dir, i),
-                os.path.join(self.work_dir, os.path.join("envs", i)),
-            )
-
-    def get_config(self):
-        """
-        get default configuration
-        """
-        config = growpi.parse_yaml(self.config_file)
-        cluster = growpi.parse_yaml(self.cluster_file)
-        return (config, cluster)
+        conf["params"]["trimming"][trimmer]["do"] = False
+        conf["params"]["rmhost"][rmhoster]["do"] = False
+    return conf
 
 
 def init(args, unknown):
     if args.workdir:
-        project = growpi_config(args.workdir)
+        project = growpi.metaconfig(args.workdir)
         print(project.__str__())
         project.create_dirs()
+        conf = project.get_config()
 
-        conf, cluster = project.get_config()
-
-        conf["envs"]["bioenv2"] = os.path.join(
-            os.path.realpath(args.workdir), "envs/bioenv2.yaml"
+        conf = update_config_tools(
+            conf, args.begin, args.trimmer, args.rmhoster, args.profiler
         )
 
         if args.samples:
             conf["params"]["samples"] = args.samples
+        else:
+            print("Please supply samples table")
+            sys.exit(-1)
 
         growpi.update_config(
             project.config_file, project.new_config_file, conf, remove=False
-        )
-        growpi.update_config(
-            project.cluster_file, project.new_cluster_file, cluster, remove=False
         )
     else:
         print("Please supply a workdir!")
@@ -204,7 +167,6 @@ def main():
     Microbial community grow rate profiling pipeline
 
 """
-
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=textwrap.dedent(banner),
@@ -225,7 +187,14 @@ def main():
         metavar="WORKDIR",
         type=str,
         default="./",
-        help="project workdir, default: ./",
+        help="project workdir",
+    )
+    common_parser.add_argument(
+        "--check-samples",
+        dest="check_samples",
+        default=False,
+        action="store_true",
+        help="check samples, default: False",
     )
 
     run_parser = argparse.ArgumentParser(add_help=False)
@@ -233,57 +202,103 @@ def main():
         "--config",
         type=str,
         default="./config.yaml",
-        help="config.yaml, default: ./config.yaml",
+        help="config.yaml",
     )
     run_parser.add_argument(
-        "--cluster",
+        "--profile",
         type=str,
-        default="./cluster.yaml",
-        help="cluster.yaml, default: ./cluster.yaml",
+        default="./profiles/slurm",
+        help="cluster profile name",
     )
     run_parser.add_argument(
-        "--cores", type=int, default=8, help="CPU cores, default: 8"
-    )
+        "--cores",
+        type=int,
+        default=240,
+        help="all job cores, available on '--run-local'")
     run_parser.add_argument(
-        "--jobs", type=int, default=80, help="qsub job numbers, default: 80"
-    )
+        "--local-cores",
+        type=int,
+        dest="local_cores",
+        default=8,
+        help="local job cores, available on '--run-remote'")
     run_parser.add_argument(
-        "--list", default=False, action="store_true", help="list pipeline rules",
-    )
+        "--jobs",
+        type=int,
+        default=30,
+        help="cluster job numbers, available on '--run-remote'")
     run_parser.add_argument(
-        "--run", default=False, action="store_true", help="run pipeline",
-    )
-    run_parser.add_argument(
-        "--debug", default=False, action="store_true", help="debug pipeline",
-    )
-    run_parser.add_argument(
-        "--dry_run", default=False, action="store_true", help="dry run pipeline",
-    )
-    run_parser.add_argument(
-        "--qsub", default=False, action="store_true", help="qsub pipeline",
-    )
-    run_parser.add_argument(
-        "--wait", type=int, default=60, help="wait given seconds, default: 60"
-    )
-    run_parser.add_argument(
-        "--use_conda", default=False, action="store_true", help="use conda environment"
-    )
-    run_parser.add_argument(
-        "--conda_create_envs_only",
+        "--list",
         default=False,
+        action="store_true",
+        help="list pipeline rules",
+    )
+    run_parser.add_argument(
+        "--debug",
+        default=False,
+        action="store_true",
+        help="debug pipeline",
+    )
+    run_parser.add_argument(
+        "--dry-run",
+        default=False,
+        dest="dry_run",
+        action="store_true",
+        help="dry run pipeline",
+    )
+    run_parser.add_argument(
+        "--run-local",
+        default=False,
+        action="store_true",
+        help="run pipeline on local computer",
+    )
+    run_parser.add_argument(
+        "--run-remote",
+        default=False,
+        action="store_true",
+        help="run pipeline on remote cluster",
+    )
+    run_parser.add_argument(
+        "--cluster-engine",
+        default="slurm",
+        choices=["slurm", "sge", "lsf", "pbs-torque"],
+        help="cluster workflow manager engine, support slurm(sbatch) and sge(qsub)"
+    )
+    run_parser.add_argument("--wait", type=int, default=60, help="wait given seconds")
+    run_parser.add_argument(
+        "--use-conda",
+        default=False,
+        dest="use_conda",
+        action="store_true",
+        help="use conda environment",
+    )
+    run_parser.add_argument(
+        "--conda-prefix",
+        default="~/.conda/envs",
+        dest="conda_prefix",
+        help="conda environment prefix",
+    )
+    run_parser.add_argument(
+        "--conda-create-envs-only",
+        default=False,
+        dest="conda_create_envs_only",
         action="store_true",
         help="conda create environments only",
     )
 
     subparsers = parser.add_subparsers(title="available subcommands", metavar="")
     parser_init = subparsers.add_parser(
-        "init", parents=[common_parser], prog="growpi init", help="init project",
+        "init",
+        formatter_class=growpi.custom_help_formatter,
+        parents=[common_parser],
+        prog="growpi init",
+        help="init project",
     )
     parser_run_wf = subparsers.add_parser(
         "run_wf",
+        formatter_class=growpi.custom_help_formatter,
         parents=[common_parser, run_parser],
         prog="growpi run_wf",
-        help="computing grow rate",
+        help="metagenome grow rate profiling pipeline",
     )
 
     parser_init.add_argument(
@@ -293,9 +308,51 @@ def main():
         default=None,
         help="""desired input:
 samples list, tsv format required.
-        the header is: [id, fq1, fq2]
+
+if begin from trimming, rmhost, or assembly:
+    if it is fastq:
+        the header is: [sample_id, fq1, fq2]
+    if it is sra:
+        the header is: [sample_id, sra]
+
+if begin from simulate:
+        the header is: [id, genome, abundance, reads_num, model]
+
 """,
     )
+    parser_init.add_argument(
+        "-b",
+        "--begin",
+        type=str,
+        default="trimming",
+        choices=["simulate", "trimming", "rmhost", "profiling"],
+        help="pipeline starting point",
+    )
+    parser_init.add_argument(
+        "--trimmer",
+        type=str,
+        default="fastp",
+        required=False,
+        choices=["oas1", "sickle", "fastp"],
+        help="which trimmer used",
+    )
+    parser_init.add_argument(
+        "--rmhoster",
+        type=str,
+        default="bowtie2",
+        required=False,
+        choices=["soap", "bwa", "bowtie2", "minimap2", "kraken2", "kneaddata"],
+        help="which rmhoster used",
+    )
+    parser_init.add_argument(
+        "--profiler",
+        type=str,
+        default="grid",
+        required=False,
+        choices=["irep", "grid", "demic"],
+        help="which profiler used",
+    )
+ 
     parser_init.set_defaults(func=init)
 
     parser_run_wf.add_argument(
